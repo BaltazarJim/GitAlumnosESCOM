b4cefdfceaa5cfed1c4885c08f039f8043bb7e45
BaltazarJim <rebajiru@gmail.com>
2023-11-22 19:31:05 -0600

    c
    c

--- Assembler/32MC202/INT0Ext.X/INT01y2.s
+++ Assembler/32MC202/INT0Ext.X/INT01y2.s
@@ -1,33 +1,11 @@
 /*
- * File:   template.s
- * Author: Baltazar Jiménez 
+ * File:   %<%NAME%>%.%<%EXTENSION%>%
+ * Author: %<%USER%>%
  *
- * Created on March 13th, 2023
+ * Created on %<%DATE%>%, %<%TIME%>%
  */
 
     .include "p33FJ32MC202.inc"
-    
-    ;User program memory is not write-protected
-    #pragma config __FGS, GWRP_OFF & GSS_OFF & GCP_OFF
-    
-    ;Internal Fast RC (FRC)
-    ;Start-up device with user-selected oscillator source
-    #pragma config __FOSCSEL, FNOSC_FRC & IESO_ON
-    
-    ;Both Clock Switching and Fail-Safe Clock Monitor are disabled
-    ;XT mode is a medium-gain, medium-frequency mode that is used to work with crystal
-    ;frequencies of 3.5-10 MHz
-  ; #pragma config __FOSC, FCKSM_CSDCMD & POSCMD_XT
-    
-    ;Watchdog timer enabled/disabled by user software
-    #pragma config __FWDT, FWDTEN_OFF
-    
-    ;POR Timer Value
-    #pragma config __FPOR, FPWRT_PWR128
-   
-    ; Communicate on PGC1/EMUC1 and PGD1/EMUD1
-    ; JTAG is Disabled
-    #pragma config __FICD, ICS_PGD1 & JTAGEN_OFF
 
 
 ;..............................................................................
@@ -48,7 +26,9 @@
                                  ;in the C file.
 
     .global __reset          ;The label for the first line of code.
-
+    .GLOBAL __INT0Interrupt
+    .GLOBAL __INT1Interrupt
+    .global __INT2Interrupt
 ;..............................................................................
 ;Constants stored in Program space
 ;..............................................................................
@@ -87,7 +67,8 @@ y_input:  .space 2*SAMPLES
 
     .section .nbss, bss, near
 var1:     .space 2               ;Example of allocating 1 word of space for
-                                 ;variable "var1".
+SERGIO: .SPACE 3
+RODRIGO: .SPACE 4;variable "var1".
 
 
 
@@ -106,79 +87,27 @@ __reset:
     CALL _wreg_init           ;Call _wreg_init subroutine
                                   ;Optionally use RCALL instead of CALL
 
-
+				  
 
 
         ;<<insert more user code here>>
 
-
-    SETM    AD1PCFGL  ;PORTB AS DIGITAL
-    CLR	    TRISB
-    MOV	    #1,   W9	    ;PARAMETER FOR DELAY_ms(w9)
-
-
-done:
-    
-    COM	PORTB
-    
-    CALL DELAY_1ms
-
-    
-BRA     done              ;Place holder for last line of executed code
-    
-    
-    
-;******************************************************************************
-;DESCRIPTION:	SECTION OF CODE FOR A 1s DELAY
-;PARAMETER: 	NINGUNO
-;RETURN: 	NINGUNO
-;******************************************************************************		    
-    
-;FOSC = 7.3725 MHz -> this is the internal clock source (FRC) and we are using it. 
-    ;If you use another clock source you have to modify "config __FOSC" section 
-    
-;FCY = FOSC/2 = 3686250 cycles p/sec.
-;T(FCY) = 271.278 ns. This is the time for an internal instruction cycle clock (FCY).	
-
-;"CYCLE1" will repeat 2^16 times = 65536 times
-;DEC(1) + BRA(2) = 3 pulses in total
-;(BRA uses 2 CLK pulses when it jumps and just one if it does not)
-
-;65536 * 3 cycles * 271 ns = 53.28 ms
-;Thus, "CYCLE1" must be repeated 19 times to delay 1s
-DELAY_1s:
-    PUSH	    W0
-    PUSH	    W1	
+CALL	CONF_PORTB
+CALL	CONF_INT0
+CALL	CONF_INT1
 	
-    MOV	    #19,	    W1
-CYCLE2:	
-    CLR	    W0
-	
-CYCLE1:		
-    DEC	    W0,		    W0
-    BRA	    NZ,		    CYCLE1
-	
-    DEC	    W1,		    W1
-    BRA	    NZ,		    CYCLE2
-	
-    POP	    W1
-    POP	    W0
-    RETURN	
+
+MOV	#250,   W9	    ;PARAMETER FOR DELAY_ms(w9)
+CLR	var1	
+done:    
+    COM.B    PORTB
+    CALL    DELAY_1ms
+    NOP
+    NOP 
+    NOP
+    NOP
     
-    
-    DELAY_1ms:
-    PUSH    W0
-    PUSH    W9	    ;ARGUMENT: HOW MANY MILISECONDS
-    DEC	    W9,	    W9
-    DO	    W9,	    B2
-	MOV	    #3682,	W0	;1ms / 271ns = 3686 pulses - some of them
-	REPEAT  W0			;;for the call and return.
-	NOP
-    B2: NOP
-    
-    POP	    W9
-    POP	    W0
-    RETURN    
+    BRA     done              ;Place holder for last line of executed code
 
 
 
@@ -193,8 +122,93 @@ _wreg_init:
     MOV W0, [++W14]
     CLR W14
     RETURN
+    
+CONF_PORTB:
+    SETM    AD1PCFGL	;PORTB AS DIGITAL
+    CLR	    TRISB	;PORTB AS OUTPUT  
+    BSET    TRISB,	#RB7 ;HERE THE EXTERNAL INT0
+    RETURN
 
+CONF_INT0:
+    BSET    INTCON1,	#NSTDIS
+    
+    BSET    IPC0,	#INT0IP2
+    BCLR    IPC0,	#INT0IP1
+    BCLR    IPC0,	#INT0IP0
+    
+    BCLR    INTCON2,	#INT0EP	    ;External Interrupt 0 Edge Detect Polarity Select bit
+    BCLR    IFS0,	#INT0IF
+    BSET    IEC0,	#INT0IE    
+    
+    RETURN	
+    
+CONF_INT1:
+    PUSH    W0    
+    
+    BSET    IPC5,	#INT1IP2
+    BCLR    IPC5,	#INT1IP1
+    BCLR    IPC5,	#INT1IP0
+    
+    BCLR    INTCON2,	#INT1EP	    ;External Interrupt 1 Edge Detect Polarity Select bit
+    BCLR    IFS1,	#INT1IF
+    BSET    IEC1,	#INT1IE 
+    
+    BSET    TRISB,	#15	    ;Ext Interrupt 1 is attachet to RB15
+    MOV	    #0X0F00,	W0
+    MOV	    W0,		RPINR0
+    
+    POP	    W0
+    RETURN	
+    
+    
+    DELAY_1ms:
+    PUSH    W0
+    PUSH    W9	    ;ARGUMENT: HOW MANY MILISECONDS
+    DEC	    W9,	    W9
+    DO	    W9,	    B2
+	MOV	    #3682,	W0	;1ms / 271ns = 3686 pulses - some of them
+	REPEAT  W0			;for the call and return.
+	NOP
+    B2: NOP
+    
+    POP	    W9
+    POP	    W0
+    RETURN    
+    
 
+__INT0Interrupt:
+    PUSH    PORTB		    ;keep safe PORTB
+    PUSH    W0
+    PUSH    W9
+    
+    MOV	    #1500,   W9
+    INC	    var1
+    MOV	    var1,   W0
+    MOV	    W0,	    PORTB
+    CALL    DELAY_1ms
+    
+    POP	    W9
+    POP	    W0
+    POP	    PORTB
+    BCLR    IFS0,	#INT0IF	    ;the user must clear the interrupt flag
+    RETFIE
+    
+__INT1Interrupt:
+    PUSH    PORTB		    ;keep safe PORTB
+    PUSH    W0
+    PUSH    W9
+    
+    MOV	    #1500,   W9
+    INC2    var1
+    MOV	    var1,   W0
+    MOV	    W0,	    PORTB
+    CALL    DELAY_1ms
+    
+    POP	    W9
+    POP	    W0
+    POP	    PORTB
+    BCLR    IFS1,	#INT1IF	    ;the user must clear the interrupt flag
+    RETFIE
 
 
 ;--------End of All Code Sections ---------------------------------------------
---
+++ Assembler/32MC202/INT0Ext.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=INT0Ext.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/INT0Ext.X.production.hex
